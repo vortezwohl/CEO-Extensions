c88522f70ca12ee44643ae697d87c8ee38603e18
吴子豪(Zihao Wu) <zihao.wu1@hirain.com>
2025-01-16 14:48:02 +0800

    feat: 添加获取当前日期时间和文件读写ability

--- ceo_ext/ability/file_access.py
+++ ceo_ext/ability/file_access.py
@@ -10,7 +10,8 @@ def read_local_file(filename: str, encoding: str = 'utf-8', *args, **kwargs) -> 
 
 
 @ability
-def write_file(filename: str, content: str, encoding: str = 'utf-8', *args, **kwargs) -> str:
+def write_local_file(filename: str, content: str, encoding: str = 'utf-8', *args, **kwargs) -> str:
+    # if the file doesn't exist, it will be created.
     with open(filename, 'w', encoding=encoding) as f:
         f.write(content)
     return f'"{content}" written to "{filename}".'
