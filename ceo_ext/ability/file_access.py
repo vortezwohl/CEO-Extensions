cce233cb0dd11061c96c1b58a4d36857a7ee6a50
吴子豪(Zihao Wu) <zihao.wu1@hirain.com>
2025-01-16 11:13:25 +0800

    feat: 添加获取当前日期时间和文件读写ability

--- ceo_ext/ability/file_access.py
+++ ceo_ext/ability/file_access.py
@@ -2,7 +2,7 @@ from ceo import ability
 
 
 @ability
-def read_local_file(filename: str, encoding: str = 'utf-8') -> str:
+def read_local_file(filename: str, encoding: str = 'utf-8', *args, **kwargs) -> str:
     content = str()
     with open(filename, 'r', encoding=encoding) as f:
         content = f.read()
@@ -10,7 +10,7 @@ def read_local_file(filename: str, encoding: str = 'utf-8') -> str:
 
 
 @ability
-def write_file(filename: str, content: str, encoding: str = 'utf-8') -> str:
+def write_file(filename: str, content: str, encoding: str = 'utf-8', *args, **kwargs) -> str:
     with open(filename, 'w', encoding=encoding) as f:
         f.write(content)
     return f'"{content}" written to "{filename}".'
