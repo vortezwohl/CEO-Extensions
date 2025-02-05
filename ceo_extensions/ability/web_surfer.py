00845a26d0c1f8f0bddb9a4b5868a1db48a5825e
吴子豪(Zihao Wu) <zihao.wu1@hirain.com>
2025-02-05 11:34:18 +0800

    feat: 添加 duckduckgo search

---
+++ ceo_extensions/ability/web_surfer.py
@@ -0,0 +1,21 @@
+from ceo import ability
+from duckduckgo_search import DDGS
+
+ddgs = DDGS()
+
+
+@ability
+def search_on_duckduckgo(keywords: list[str], max_results_for_each_keywords: int = 5):
+    # search on duckduckgo if you don't know clearly about something
+    # you can search several keywords at the same time with `search_on_duckduckgo`
+    results = dict()
+    for kw in keywords:
+        results[f'search for "{kw}"'] = ddgs.text(
+            keywords=kw,
+            region='wt-wt',
+            safesearch='off',
+            timelimit=None,
+            backend='auto',
+            max_results=max_results_for_each_keywords
+        )
+    return results
