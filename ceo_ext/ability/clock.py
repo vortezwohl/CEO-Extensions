b7786dcb54cd747d274af99546d0720768ec4f7d
吴子豪(Zihao Wu) <zihao.wu1@hirain.com>
2025-02-05 10:20:33 +0800

    fix: 优化 get_current_datetime()

--- ceo_ext/ability/clock.py
+++ ceo_ext/ability/clock.py
@@ -4,4 +4,5 @@ from ceo import ability
 
 @ability
 def get_current_datetime(*args, **kwargs) -> str:
+    # `get_current_datetime` provides the current time to the user
     return str(datetime.datetime.now())
