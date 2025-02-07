2d81ccc54a5fe57a69cdba8ec795416ae4955fef
吴子豪(Zihao Wu) <zihao.wu1@hirain.com>
2025-02-07 10:40:43 +0800

    fix: 删除 clock 模块，ceo 不需要额外的ability来获取时间

--- ceo_extensions/ability/clock.py
+++
@@ -1,8 +0,0 @@
-import datetime
-from ceo import ability
-
-
-@ability
-def get_current_datetime(*args, **kwargs) -> str:
-    # `get_current_datetime` provides the current time to the user
-    return str(datetime.datetime.now())
