612c1c701a0773aabd8af06d3d98c2cc0c96f635
吴子豪(Zihao Wu) <zihao.wu1@hirain.com>
2025-02-07 11:02:13 +0800

    fix: 优化 search on ddg

--- ceo_extensions/ability/web_surfer.py
+++ ceo_extensions/ability/web_surfer.py
@@ -10,7 +10,7 @@ def search_on_duckduckgo(keywords: list[str], max_results_for_each_keywords: int
     # you can search several keywords at the same time with `search_on_duckduckgo`
     results = dict()
     for kw in keywords:
-        results[f'search for "{kw}"'] = ddgs.text(
+        search_results = ddgs.text(
             keywords=kw,
             region='wt-wt',
             safesearch='off',
@@ -18,4 +18,14 @@ def search_on_duckduckgo(keywords: list[str], max_results_for_each_keywords: int
             backend='auto',
             max_results=max_results_for_each_keywords
         )
+        for _res in search_results:
+            _title = _res.get('title', 'unknown')
+            _res.pop('title')
+            _link = _res.get('href', 'unknown')
+            _res.pop('href')
+            _res['source'] = f'({_title})[{_link}]'
+            _content = _res.get('body')
+            _res.pop('body')
+            _res['content'] = _content
+        results[f'search results for "{kw}"'] = search_results
     return results
